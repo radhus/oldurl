4b451e572baef9fa1472d7d2d007fb4d9af21210
William Johansson <radar@radhuset.org>
2016-09-23 01:26:55 +0200

    Refactor oldurl to provide message function

--- oldurl.py
+++ oldurl.py
@@ -11,20 +11,16 @@ FORMAT = ("@%(nick)s OLD! "
           "@%(found)s posted this ~%(since)s ago (%(date)s) "
           ":feelsbadman:")
 
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Need nick and text arguments")
-        sys.exit(1)
+# < prefix and > suffix can be added by Slack
+PATTERN = re.compile(r"<?(https?://[^\s\>]+)")
 
-    args = {"nick": sys.argv[1]}
-    text = sys.argv[2]
-    connect()
 
-    PATTERN = re.compile(r"(https?://[^\s]+)")
+def old_message(nick, text):
+    args = {"nick": nick}
 
     match = PATTERN.findall(text)
     if not match:
-        sys.exit(0)
+        return None
 
     now = datetime.now()
     for url in match:
@@ -35,12 +31,27 @@ if __name__ == '__main__':
                 args["found"] = find.nick
                 args["date"] = find.date.strftime("%Y-%m-%d %H:%M:%S")
                 args["since"] = prettydate.pretty_datedelta(find.date, now)
-                print(FORMAT % args)
+                return FORMAT % args
             else:
                 # store it
                 u = URL(url=trimmed,
                         nick=args["nick"],
                         date=now)
                 u.save()
+                return None
         except:
-            sys.exit(1)
+            return None
+
+    return None
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Need nick and text arguments")
+        sys.exit(1)
+
+    connect()
+
+    msg = old_message(sys.argv[1], sys.argv[2])
+    if msg:
+        print(msg)
